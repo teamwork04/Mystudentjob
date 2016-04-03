ccad31d3fffb93dfe2c65783e10dcb7f29d9857f
Mamta_Kamboj <mamtakamboj16@gmail.com>
2016-04-03 23:52:22 +0100

    First Commit..

---
+++ Mystudentjob.ie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mystudentjob.ie.MvcApplication" Language="C#" %>
---
+++ Mystudentjob.ie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mystudentjob.ie.Startup))]
+namespace Mystudentjob.ie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
